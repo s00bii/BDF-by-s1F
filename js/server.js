933cef3d0357dcb5e4dcb12c751e30fd2e9611ef
Subere Houssein Ali <subere.h@gmail.com>
2024-12-31 03:48:15 -0500

    change so its binded to only 0.0.0.0

--- js/server.js
+++ js/server.js
@@ -28,5 +28,5 @@ app.get("/proxy-image", async (req, res) => {
 
 const PORT = 5000;
 app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on http://0.0.0.0:${PORT}`);
+    console.log(`Proxy server running on http://0.0.0.0:${PORT}`);
 });
