a6c36174a09bc6c803ac3d7a16565cbc88359f9f
Subere Houssein Ali <subere.h@gmail.com>
2024-12-31 03:25:34 -0500

    crash out loading ...

--- js/server.js
+++ js/server.js
@@ -1,9 +1,6 @@
-console.log("Node.js version:", process.version);
-console.log("node-fetch version:", require("node-fetch/package.json").version);
-console.log("PM2 environment:", process.env);
-
 const express = require("express");
 const cors = require("cors");
+const fetch = require("node-fetch");
 
 const app = express();
 app.use(cors());
@@ -15,7 +12,6 @@ app.get("/proxy-image", async (req, res) => {
     }
 
     try {
-        const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fetch(...args));
         const response = await fetch(imageUrl);
         const buffer = await response.buffer();
         res.set("Content-Type", "image/jpeg");
