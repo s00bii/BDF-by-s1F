2756cd2899a9c7dbc3022a9d3078c2a316e93a4d
Subere Houssein Ali <subere.h@gmail.com>
2025-01-02 00:07:05 -0500

    removed some stuff from the server

--- js/server.js
+++ js/server.js
@@ -1,15 +1,8 @@
 const express = require("express");
 const fs = require("fs");
 const https = require("https");
-const http = require("http");
 
 const app = express();
-
-// Remove this HTTP listen on port 80
-// http.createServer((req, res) => {
-//     res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
-//     res.end();
-// }).listen(80);
 
 // HTTPS server options
 const options = {
@@ -18,15 +11,10 @@ const options = {
     ca: fs.readFileSync("/etc/letsencrypt/live/s1ckfit.com/chain.pem"),
 };
 
-// Start HTTPS server (for secure traffic)
-https.createServer(options, app).listen(5001, () => {
+// Start HTTPS server (for secure traffic) and listen on port 5001
+https.createServer(options, app).listen(5001, '0.0.0.0', () => {
     console.log("Server running on https://s1ckfit.com:5001");
 });
-
-//const PORT = 5001; // Node.js should listen on this port
-//app.listen(PORT, () => {
-//    console.log(`Node.js server running on port ${PORT}`);
-//});
 
 // Example route
 app.get("/", (req, res) => {
